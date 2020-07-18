a07234c49daf47404daf78eb426873a19501dfa8
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-18 00:46:50 +0000

    general CLI layout complete

--- lib/nat_parks/cli.rb
+++ lib/nat_parks/cli.rb
@@ -6,30 +6,55 @@ require 'nokogiri'
 
 class NatParks::CLI
   def call
-    puts "Welcome to NatParks! Are you a Delware resident? [ y / n ]"
+    puts "Welcome to NatParks! Are you a Pennsylvania, Delware or New Jersey resident? [ y / n ]"
     answer = gets.chomp
       if answer == "y"
-        puts "Great! Here are all of the National Parks and Historic Trails in your home-state!"
+        puts "Great! Here's the list of available states to explore!"
+        choices
       else 
-        puts "My apologies. At the moment you must be a Delaware resident to use NatParks."
+        puts "My apologies. At the moment you must located in one of the 3 states, previously disclosed, to use NatParks."
         exit
       end
   end
   
   def choices
-    puts "Which park/trail would you like to learn more about?"
-    puts "\n1. ... \n2. ... \n3. ..."
-    input = gets.chomp 
+    print_states
+    input = nil
     
-    case input
+    while input != "exit"
+    
+    puts "Enter the number [1 - 3] of the state you'd like to dive deeper into."
+    puts "Enter 'list' to see list of state."
+    puts "Enter 'exit' to exit."
       
+    input = gets.chomp
+      
+      case input
+        when "1"
+          puts "\nParks and Trails in PA"
+        when "2"
+          puts "\nParks and Trails in DE"
+        when "3"
+          puts "\nParks and Trails in NJ"
+        when "list"
+          print_states
+        end
+      end
+      if input == "exit"
+        exit
+      end
+    end
+    
+    def print_states
+      puts "1. PA"
+      puts "2. DE"
+      puts "3. NJ"
     end
   end
-end
 
-module NatParks
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
+# module NatParks
+#  module Cli
+#   class Error < StandardError; end
+#    # Your code goes here...
+#  end
+# end
